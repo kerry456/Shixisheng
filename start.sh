9e987e7131498e8d42ecac0d3a3ebdd794a53b32
fly_fish <35325384+kerry456@users.noreply.github.com>
2018-09-25 13:05:58 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export PATH=/usr/local/python36/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:$PATH
+#python3 /soft/demo/master/shixisheng/run.py
+cd /soft/demo/master/shixisheng/
+
+scrapy crawl master  
+
+
+
